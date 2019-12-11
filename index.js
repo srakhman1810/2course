10ea421e06345eb528b3b594f0fc785513a61d75
Rakhmanova Sevara <rakhman992@gmail.com>
2019-12-11 22:10:46 +0300

    подправлено по комментариям

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const link = document.querySelector('.link-menu'),
        menu = document.querySelector('.menu'),
        x = document.querySelector('.x-close');
-
+      
 link.addEventListener ('click', function(e) {
  menu.style.display = 'flex'; 
+ document.body.style.overflow = "hidden";
 });
 
 x.addEventListener ('click' , function(e) {
     menu.style.display = 'none';
+    document.body.style.overflow = "scroll";
 });
+
+
