4839ac260d1bc786e50a8218dd4d9306ce596f67
Rakhmanova Sevara <rakhman992@gmail.com>
2019-12-22 21:32:17 +0300

    сделана карта

--- map.js
+++ map.js
@@ -1,33 +1,5 @@
 // карты 
 ymaps.ready(init);
-var placemarks = [
-    {
-        latitude: 55.76,
-        longitude: 37.59,
-        hintContent: 'мы тут!',
-        balloonContent: '<div class="balloon"> улица Гранатовый переулок, 10.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-    },
-    {
-        latitude: 55.78,
-        longitude: 37.60,
-        hintContent: 'мы тут!',
-        balloonContent: '<div class="balloon"> улица Новослободская,11.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-    },
-    {
-        latitude: 55.76,
-        longitude: 37.57,
-        hintContent: 'мы тут!',
-        balloonContent: '<div class="balloon"> улица Заморева, 19.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-    },
-    {
-        latitude: 55.76,
-        longitude: 37.63,
-        hintContent: 'мы тут!',
-        balloonContent: '<div class="baloon"> улица Мясницкая,6.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-    }
-],
-
-    geoObjects = [];
 
 function init() {
     var map = new ymaps.Map('map', {
@@ -37,91 +9,60 @@ function init() {
         behaviors: ['drag']
     });
 
-    for (var i = 0; i < placemarks.length; i++) {
-        var geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
-            {
-                hintContent: placemarks[i].hintContent,
-                balloonContent: placemarks[i].balloonContent.join('')
-            },
-            {
-                iconLayout: 'default#image',
-                iconImageHref: './img/ninth/2.png',
-                iconImageSize: [46, 57],
-                iconImageOffset: [-23, -57]
-            }
-        );
-    }
-    var clusterer = new ymaps.Clusterer({
-        clusterIcons: [
-            {
-                href: './img/ninth/2.png',
-                size: [100, 100],
-                offset: [-50, -50]
-            }
-        ],
+    var placemark = new ymaps.Placemark([55.76, 37.59], {
+        hintContent: 'мы тут!',
+        balloonContent: '<div class="balloon"> улица Гранатовый переулок, 10.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
+    },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: './img/ninth/2.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        }
+    );
 
-        clusterIconContentLayout: null
-    });
+    var placemark1 = new ymaps.Placemark([55.78, 37.60], {
+        hintContent: 'мы тут!',
+        balloonContent: '<div class="balloon"> улица Новослободская,11.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
+
+    },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: './img/ninth/2.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        }
+    );
+
+    var placemark2 = new ymaps.Placemark([55.76, 37.57], {
+        hintContent: 'мы тут!',
+        balloonContent: '<div class="balloon"> улица Заморева, 19.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
+    },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: './img/ninth/2.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        }
+    );
+
+    var placemark3 = new ymaps.Placemark([55.76, 37.63], {
+        hintContent: 'мы тут!',
+        balloonContent: '<div class="baloon"> улица Мясницкая,6.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
+    },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: './img/ninth/2.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        }
+    );
 
     map.geoObjects.add(placemark);
-    clusterer.add(geoObjects);
-}
+    map.geoObjects.add(placemark1);
+    map.geoObjects.add(placemark2);
+    map.geoObjects.add(placemark3);
+
+}  
 
 
-
-
-
-
-// var placemark = new ymaps.Placemark([55.76, 37.59], {
-//     hintContent: 'мы тут!',
-//     balloonContent: '<div class="balloon"> улица Гранатовый переулок, 10.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-// },
-//     {
-//         iconLayout: 'default#image',
-//         iconImageHref: './img/ninth/2.png',
-//         iconImageSize: [46, 57],
-//         iconImageOffset: [-23, -57]
-//     }
-// );
-
-// var placemark1 = new ymaps.Placemark([55.78, 37.60], {
-//     hintContent: 'мы тут!',
-//     balloonContent: '<div class="balloon"> улица Новослободская,11.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-
-// },
-//     {
-//         iconLayout: 'default#image',
-//         iconImageHref: './img/ninth/2.png',
-//         iconImageSize: [46, 57],
-//         iconImageOffset: [-23, -57]
-//     }
-// );
-
-// var placemark2 = new ymaps.Placemark([55.76, 37.57], {
-//     hintContent: 'мы тут!',
-//     balloonContent: '<div class="balloon"> улица Заморева, 19.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-// },
-//     {
-//         iconLayout: 'default#image',
-//         iconImageHref: './img/ninth/2.png',
-//         iconImageSize: [46, 57],
-//         iconImageOffset: [-23, -57]
-//     }
-// );
-
-// var placemark3 = new ymaps.Placemark([55.76, 37.63], {
-//     hintContent: 'мы тут!',
-//     balloonContent: '<div class="baloon"> улица Мясницкая,6.</br> График работы </br> с ПН-ПТ: 9-18:00</br> СБ-ВС: выходные дни </div>'
-// },
-//     {
-//         iconLayout: 'default#image',
-//         iconImageHref: './img/ninth/2.png',
-//         iconImageSize: [46, 57],
-//         iconImageOffset: [-23, -57]
-//     }
-// );
-// map.geoObjects.add(placemark);
-// map.geoObjects.add(placemark1);
-// map.geoObjects.add(placemark2);
-// map.geoObjects.add(placemark3);
-// }
