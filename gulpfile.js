3b88d63169ff8b4f8c54a9e54599ecc86c8a307f
Rakhmanova Sevara <rakhman992@gmail.com>
2019-12-26 16:13:06 +0300

    вот что есть

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const {src , dest ,task , series, watch } = require("gulp");
+const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+// const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+task('copy', ()=>{
+    return src('src/styles/**/*.css').pipe(dest('dist'));
+    // npm run gulp copy
+});
+
+// function copy(){
+//     return src('src/styles/**/*.css').pipe(dest('dist'))
+// }
+// exports.copy = copy
+
+task('clean', ()=>{
+    return src('dist/**/*', {read:false}).pipe(rm());
+    // npm install --save-dev gulp-rm есть 
+    // npm run gulp clean
+});
+
+const style = [
+   './node_modules/normalize.css/normalize.css',
+   // npm install --save normalize.css есть
+   './src/styles/**/*.scss'
+]
+
+task('style', ()=>{
+    return src(style)
+    .pipe(concat('main.scss'))
+// // npm install --save-dev gulp-concat есть
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist'));
+    // npm install node-sass gulp-sass --save-dev есть
+    // npm run gulp style
+});
+
+// gulp.task('browser-sync',()=> {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./dist"
+//         }
+//     });
+// });
+
+// watch("./src/styles/**/*.scss", series(style));
+task('default', series('clean' , 'style'));
+//  npm run gulp
+
+
+// сервер
+// npm install browser-sync --save-dev
+
+
+
