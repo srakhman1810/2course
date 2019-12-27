4f094aa368e3318a7948c4450bb0a380ea4176c4
Rakhmanova Sevara <rakhman992@gmail.com>
2019-12-27 04:14:39 +0300

    установка gulp 1 часть

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,15 @@
-const {src , dest ,task , series, watch } = require("gulp");
+const {src , dest, task, series, watch} = require("gulp");
 const rm = require('gulp-rm');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-// const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
 task('copy', ()=>{
-    return src('src/styles/**/*.css').pipe(dest('dist'));
+    return src('src/styles/**/*.scss').pipe(dest('dist'));
     // npm run gulp copy
 });
-
-// function copy(){
-//     return src('src/styles/**/*.css').pipe(dest('dist'))
-// }
-// exports.copy = copy
 
 task('clean', ()=>{
     return src('dist/**/*', {read:false}).pipe(rm());
@@ -23,36 +18,34 @@ task('clean', ()=>{
 });
 
 const style = [
-   './node_modules/normalize.css/normalize.css',
+   'node_modules/normalize.css/normalize.css',
    // npm install --save normalize.css есть
-   './src/styles/**/*.scss'
+   'src/styles/main.scss'
 ]
 
 task('style', ()=>{
     return src(style)
     .pipe(concat('main.scss'))
-// // npm install --save-dev gulp-concat есть
+     // npm install --save-dev gulp-concat есть
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('dist'));
     // npm install node-sass gulp-sass --save-dev есть
     // npm run gulp style
 });
 
-// gulp.task('browser-sync',()=> {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./dist"
-//         }
-//     });
-// });
+// // gulp.task('browser-sync',()=> {
+// //     browserSync.init({
+// //         server: {
+// //             baseDir: "./dist"
+// //         }
+// //     });
+// // });
 
-// watch("./src/styles/**/*.scss", series(style));
+watch("./src/styles/**/*.scss", series('style'));
 task('default', series('clean' , 'style'));
-//  npm run gulp
+// //  npm run gulp
 
-
-// сервер
-// npm install browser-sync --save-dev
-
+// // сервер
+// // npm install browser-sync --save-dev
 
 
